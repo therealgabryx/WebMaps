f78c61d05847e6ea332627cca743451b6067d40b
therealgabryx <gabriele.concli@gmail.com>
2020-11-12 23:56:19 +0000

    added map controls and navigation

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react'
 import './index.css'
 
+// Mapbox directions
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+
 export default function Index() {
-    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    let mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
     mapboxgl.accessToken = 'pk.eyJ1IjoidGhlcmVhbGdhYnJ5eCIsImEiOiJja2hmZXdjYmcwN3BmMnlxcTI3M3FlczIzIn0.JHdtdPNZbQmPNRxIO1T0Yw';
 
     useEffect(() => {        
@@ -15,18 +19,28 @@ export default function Index() {
     } 
 
     function errorLocation() {
-        const ref = document.querySelector('.Map')
-        ref.innerHTML = 'error'
+        setupMap([ 12.5674, 41.8719 ])
     } 
 
     function setupMap(center) {
         
         let map = new mapboxgl.Map({
           container: 'Map',
-          style: 'mapbox://styles/mapbox/streets-v11',
+          style: 'mapbox://styles/mapbox/dark-v10',
           center: center,
           zoom: 16
         });
+
+        const nav = new mapboxgl.NavigationControl();
+        map.addControl(nav, 'top-right');
+
+        var directions = new MapboxDirections({
+            accessToken: mapboxgl.accessToken,
+            unit: 'metric',
+            profile: 'mapbox/driving'
+        });
+        
+        map.addControl(directions, 'top-left');
     } 
 
     return (
